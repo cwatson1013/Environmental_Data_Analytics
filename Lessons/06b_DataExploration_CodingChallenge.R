83eb2aefb44a703504920010f288b77e8c73ab23
Caroline Watson <carolinewatson@Carolines-MacBook-Pro.local>
2019-01-31 11:39:19 -0500

    Completed assingment 3

--- Lessons/06b_DataExploration_CodingChallenge.R
+++ Lessons/06b_DataExploration_CodingChallenge.R
@@ -5,31 +5,43 @@
 ## Instructions listed in comments throughout the script.
 
 #### Setup ----
-setwd("/Users/katerisalk/Documents/Duke/Courses/Environmental_Data_Analytics")
+setwd("/Users/carolinewatson/Documents/Spring 2019/Environmental Data Analytics/Env_Data_Analytics/Assignments")
 
 library("tidyverse")
 
-ecotox.neonic <- read.csv("/Data/Raw/ECOTOX_Neonicotinoids_Mortality_raw.csv")
+ecotox.neonic <- read.csv("./Data/Raw/ECOTOX_Neonicotinoids_Mortality_raw.csv")
 
 #### Basic Data Summaries ----
-head(ecotox.neonic,)
-summary(ecotox.neonic%Chemical.Name)
-summary(ecotox.neonic%Pub..Year)
+head(ecotox.neonic)
+summary(ecotox.neonic$Chemical.Name)
+summary(ecotox.neonic$Pub..Year)
 
 # Fix formatting of column names (spaces originally present were turned to periods upon import)
-colnames(ecotox.neonic)[8:12] <- c(Duration, Conc.Type, Conc.Mean, Conc.Units, Pub.Year)
+colnames(ecotox.neonic)[8:12] <- c("Duration", "Conc.Type", "Conc.Mean", "Conc.Units", "Pub.Year")
 
 # Plot histogram of counts of publication years
-ggplot(ecotox.neonic, aes(x = Pub.Year)) 
-  geom_histogram()
+ggplot(ecotox.neonic) +
+  geom_histogram(aes(x = Pub.Year))
 
 # Plot histogram of counts chemical names
 # hint: what is the class of Chemical.Name? There are two options for a solution.
-ggplot(ecotox.neonic, x = Chemical.Name) +
-  geom_histogram()
+ecotox.neonic$Chemical.Name <- as.numeric(ecotox.neonic$Chemical.Name)
+class(ecotox.neonic$Chemical.Name)
+
+ggplot(ecotox.neonic) +
+  geom_histogram(aes(x = Chemical.Name))
 
 # Plot frequency polygon of publication years divided by chemical name
 # Define colors as something other than ggplot default 
 ggplot(ecotox.neonic) +
-  geom_freqpoly(aes(x = Pub.Year, color = Chemical.Name) +
+  geom_freqpoly(aes(x = Pub.Year, color = "red", bins = 20)) +
+  geom_freqpoly(aes(x = Pub.Year, color = "purple", bins = 20)) +
+  geom_freqpoly(aes(x = Pub.Year, color = "blue", bins = 20))
   theme(legend.position = "right")
+  
+  ggplot(ecotox.neonic) + 
+  geom_freqpoly(aes(x = Pub.Year, color = "blue")) +
+  geom_freqpoly(aes(x = Pub.Year, color = "purple"))
+  theme(legend.position = "right")
+  
+  ##still don't understand how to do this
